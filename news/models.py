b21e15a08d6ea65a8cc94d146142324c6fdfb0fd
Yokota Yoshinori <yokota@yynet.org>
2025-10-14 11:06:19 +0900

    Refactoring for news

--- news/models.py
+++ news/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.conf import settings
 
+
 class Article(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     url = models.URLField('記事URL', unique=True, max_length=1024)
@@ -11,20 +12,25 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+
 class SentArticleLog(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     sent_at = models.DateTimeField('配信日時', auto_now_add=True)
+
     class Meta:
         unique_together = ('user', 'article')
+
 
 class ClickLog(models.Model):
     """
     ユーザーの記事クリックを記録するモデル
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     clicked_at = models.DateTimeField('クリック日時', auto_now_add=True)
 
--- news/views.py
+++ news/views.py
@@ -3,13 +3,14 @@ from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Article, ClickLog
 
+
 class TrackClickView(LoginRequiredMixin, View):
     def get(self, request, pk, *args, **kwargs):
         # どの記事がクリックされたかを取得
         article = get_object_or_404(Article, pk=pk)
-        
+
         # クリックログを記録 (既に存在する場合は重複させない)
         ClickLog.objects.get_or_create(user=request.user, article=article)
-        
+
         # ユーザーを本来の記事URLにリダイレクト
         return redirect(article.url)
