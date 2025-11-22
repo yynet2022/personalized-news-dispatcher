5d624e8b56da6283ebf276e23a434abe3b51fa63
Yokota Yoshinori <yokota@yynet.org>
2025-11-22 18:31:04 +0900

    feat(news): Allow anonymous access to TrackClickView and conditional ClickLog

--- news/views.py
+++ news/views.py
@@ -4,13 +4,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Article, ClickLog
 
 
-class TrackClickView(LoginRequiredMixin, View):
+class TrackClickView(View):
     def get(self, request, pk, *args, **kwargs):
         # どの記事がクリックされたかを取得
         article = get_object_or_404(Article, pk=pk)
 
-        # クリックログを記録 (既に存在する場合は重複させない)
-        ClickLog.objects.get_or_create(user=request.user, article=article)
+        if request.user.is_authenticated:
+            # クリックログを記録 (既に存在する場合は重複させない)
+            ClickLog.objects.get_or_create(user=request.user, article=article)
 
         # ユーザーを本来の記事URLにリダイレクト
         return redirect(article.url)
