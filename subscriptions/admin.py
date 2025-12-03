da7ecf26bf88bbddf874c66475777e6da159ad8a
Yokota Yoshinori <yokota@yynet.org>
2025-12-04 08:00:11 +0900

    feat(admin): Improve Django admin panel usability
    - Customize list_display, search_fields, list_filter, aw_id_fields, and ilter_horizontal for LargeCategory, UniversalKeywords, CurrentKeywords, RelatedKeywords, QuerySet models in subscriptions/admin.py.
    - Change QuerySetAdmin search field from user__username to user__email and add user to list_filter.
    - Customize list_display, search_fields, list_filter, and aw_id_fields for Article, SentArticleLog, ClickLog models in
    ews/admin.py.
    - Add published_date to ArticleAdmin's list_filter.
    - Remove rticle from SentArticleLogAdmin and ClickLogAdmin's list_filter, and add sent_at and clicked_at respectively.

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -8,9 +8,55 @@ from .models import (
     QuerySet
 )
 
-# モデルをAdminサイトに登録
-admin.site.register(LargeCategory)
-admin.site.register(UniversalKeywords)
-admin.site.register(CurrentKeywords)
-admin.site.register(RelatedKeywords)
-admin.site.register(QuerySet)
+
+@admin.register(LargeCategory)
+class LargeCategoryAdmin(admin.ModelAdmin):
+    """
+    大分類モデルの管理サイト設定
+    """
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(UniversalKeywords)
+class UniversalKeywordsAdmin(admin.ModelAdmin):
+    """
+    普遍キーワードモデルの管理サイト設定
+    """
+    list_display = ('name', 'large_category', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('large_category',)
+
+
+@admin.register(CurrentKeywords)
+class CurrentKeywordsAdmin(admin.ModelAdmin):
+    """
+    時事キーワードモデルの管理サイト設定
+    """
+    list_display = ('name', 'large_category', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('large_category',)
+
+
+@admin.register(RelatedKeywords)
+class RelatedKeywordsAdmin(admin.ModelAdmin):
+    """
+    関連キーワードモデルの管理サイト設定
+    """
+    list_display = ('name', 'large_category', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('large_category',)
+
+
+@admin.register(QuerySet)
+class QuerySetAdmin(admin.ModelAdmin):
+    """
+    クエリセットモデルの管理サイト設定
+    """
+    list_display = ('name', 'user', 'large_category', 'country', 'auto_send')
+    search_fields = ('name', 'user__email')
+    list_filter = ('user', 'auto_send', 'large_category', 'country')
+    raw_id_fields = ('user',)
+    filter_horizontal = (
+        'universal_keywords', 'current_keywords', 'related_keywords'
+    )
